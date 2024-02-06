b2eba2188e8e214b16d4185cbc1e38343fc4b4a2
Harry Day <contact@harryday.xyz>
2024-02-06 21:01:35 +0000

    Add autoformat on save

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -23,6 +23,8 @@ local on_attach = function(_, bufnr)
     vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
         vim.lsp.buf.format()
     end, { desc = 'Format current buffer with LSP' })
+
+    vim.cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.format()]]
 end
 
 -- mason-lspconfig requires that these setup functions are called in this order
