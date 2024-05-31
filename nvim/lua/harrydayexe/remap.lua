725867662fc9672154b050b8c273524a320dab73
Harry Day <contact@harryday.xyz>
2024-05-31 17:17:43 +0100

    add autoclose and oil

--- nvim/lua/harrydayexe/remap.lua
+++ nvim/lua/harrydayexe/remap.lua
@@ -1,6 +1,6 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = '[P]roject [V]iew' })
+vim.keymap.set("n", "<leader>pv", "<CMD>Oil<CR>", { desc = '[P]roject [V]iew' })
 
 -- Move visual block up or down
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
