8b2523f7696799561a4a0cfe8a4ddd1576a3da87
Harry Day <contact@harryday.xyz>
2024-10-12 17:40:33 +0100

    fix(vim): Show hidden files in telescope

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,27 +1,27 @@
 require('telescope').setup {
-  defaults = {
-    mappings = {
-      i = {
-        ['<C-u>'] = false, -- Set C-u to clear the prompt instead of scroll up
-        ['<C-d>'] = false, -- Set C-d to delete the buffer?
-      },
+    defaults = {
+        mappings = {
+            i = {
+                ['<C-u>'] = false, -- Set C-u to clear the prompt instead of scroll up
+                ['<C-d>'] = false, -- Set C-d to delete the buffer?
+            },
+        },
     },
-  },
 }
-
 -- Enable telescope fzf native, if installed
 pcall(require('telescope').load_extension, 'fzf')
 
 
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>fp', builtin.find_files, { desc = '[F]ind [P]roject files' })
+vim.keymap.set('n', '<leader>fp',
+    "<cmd>lua require'telescope.builtin'.find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git' }})<cr>",
+    { desc = '[F]ind [P]roject files' })
 vim.keymap.set('n', '<leader>fg', builtin.git_files, { desc = '[F]ind [G]it files' })
 vim.keymap.set('n', '<leader>/', function()
-  -- You can pass additional configuration to telescope to change theme, layout, etc.
-  require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-    winblend = 10,
-    previewer = false,
-  })
+    -- You can pass additional configuration to telescope to change theme, layout, etc.
+    require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+        winblend = 10,
+        previewer = false,
+    })
 end, { desc = '[/] Fuzzily search in current buffer' })
 vim.keymap.set('n', '<leader>fs', builtin.live_grep, { desc = '[F]ind by [S]earch' })
-
