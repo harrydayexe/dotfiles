8f88de0cbc5d77f5d15225a48ac499a63f14ab99
Harry Day <contact@harryday.xyz>
2025-02-01 21:30:53 +0000

    feat(nvim): Add carbon-now.nvim

---
+++ .config/nvim/after/plugin/carbon-now.lua
@@ -0,0 +1,22 @@
+require("carbon-now").setup({
+    base_url = "https://carbon.now.sh/",
+    options = {
+        bg = "rgba(175,224,230,1)",
+        drop_shadow_blur = "68px",
+        drop_shadow = true,
+        drop_shadow_offset_y = "20px",
+        font_family = "Hack",
+        font_size = "14px",
+        line_height = "133%",
+        line_numbers = false,
+        theme = "one-dark",
+        titlebar = "",
+        watermark = false,
+        width = "680",
+        window_theme = "none",
+        padding_horizontal = "56px",
+        padding_vertical = "56px",
+    },
+})
+
+vim.keymap.set("v", "<leader>cn", ":CarbonNow<CR>", { silent = true })
