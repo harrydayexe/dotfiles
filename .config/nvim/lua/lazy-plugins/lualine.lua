37423c79c16dffd983c0b89c7ca5cfcbaa13d5a1
Harry Day <contact@harryday.xyz>
2025-10-28 17:53:26 +0000

    feat(nvim): status line shows folder in oil

--- .config/nvim/lua/lazy-plugins/lualine.lua
+++ .config/nvim/lua/lazy-plugins/lualine.lua
@@ -9,5 +9,29 @@ return {
       component_separators = '|',
       section_separators = ' ',
     },
+    sections = {
+      lualine_c = {
+        {
+          'filename',
+          path = 0,
+          cond = function()
+            return vim.bo.filetype ~= 'oil'
+          end,
+        },
+        {
+          function()
+            local oil = require('oil')
+            local dir = oil.get_current_dir()
+            if dir then
+              return vim.fn.fnamemodify(dir, ':~')
+            end
+            return ''
+          end,
+          cond = function()
+            return vim.bo.filetype == 'oil'
+          end,
+        },
+      },
+    },
   },
 }
