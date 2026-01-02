68cfbf80057bce36a70e52aaa7fd4428271d0976
Harry Day <contact@harryday.xyz>
2026-01-02 18:46:11 +0000

    feat(nvim): remove copilot

--- .config/nvim/after/plugin/copilot.lua
+++
@@ -1,3 +0,0 @@
-vim.keymap.set('n', '<leader>ce', '<cmd>Copilot enable<cr>', { desc = '[C]opilot [E]nable', silent = true })
-vim.keymap.set('n', '<leader>cd', '<cmd>Copilot disable<cr>', { desc = '[C]opilot [D]isable', silent = true })
-vim.keymap.set('n', '<leader>cp', '<cmd>Copilot panel<cr>', { desc = '[C]opilot [P]anel', silent = true })
--- .config/nvim/lua/lazy-plugins/init.lua
+++ .config/nvim/lua/lazy-plugins/init.lua
@@ -3,7 +3,6 @@ return {
     "tpope/vim-fugitive",
     { 'folke/which-key.nvim',  opts = {} },
     { 'numToStr/Comment.nvim', opts = {} },
-    'github/copilot.vim',
     'eandrju/cellular-automaton.nvim',
     { 'echasnovski/mini.icons', tag = 'v0.14.0' }
 }
